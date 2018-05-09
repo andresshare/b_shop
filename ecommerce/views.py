df111695b615dc97502e283264ec63b162b53064
andresshare <andresshare@gmail.com>
2018-05-08 20:12:50 -0500

    add template about && contact

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 
 
 def home_page(request):
-	return render(request,"home_page.html", {})
+	context={
+	 	"title":"message: from views.py home_page",
+	 	"content":"Welcome to the homepage"
+	}
+	return render(request, "home_page.html", context)
+
+def about_page(request):
+	context={
+	 	"title":"message: from views.py about_page",
+	 	"content":"Welcome to the aboutpage"
+	}
+	return render(request, "about_page.html", context)
+
+
+def contact_page(request):
+	context={
+	 	"title":"message: from views.py contact_page",
+	 	"content":"Welcome to the contactpage"
+	}
+	return render(request, "contact_page.html", context)
+
+

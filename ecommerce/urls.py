9e28b7a0b6857447a1d48a8b7a92bf9db179885a
andresshare <andresshare@gmail.com>
2018-05-15 15:34:19 -0500

    charge_image &&modify urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls import url
 from django.contrib import admin
 
 #Product
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view,ProductDetailView,product_detail_view
 
-from .views import home_page, about_page, contact_page,login_page,register_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
     url(r'^$',home_page),
@@ -32,6 +32,8 @@ urlpatterns = [
     url(r'^register/',register_page),
     url(r'^products/',ProductListView.as_view()),
     url(r'^products-fbv/',product_list_view),
+    url(r'^products/(?P<pk>\d+)/$',ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$',product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
 
